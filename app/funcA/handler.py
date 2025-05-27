43100245dffc7362bf79257af9a66d7981ac6820
Victor Pan <victor.pan@hagertyconsulting.com>
2025-05-27 01:04:06 -0400

    test

--- app/funcA/handler.py
+++ app/funcA/handler.py
@@ -1,17 +1,11 @@
+from pyfiglet import Figlet
+
 def lambda_handler(event, context):
-    n = 3
-    grid_lines = []
 
-    for _ in range(n):
-        grid_lines.append("+---" * n + "+")
-        grid_lines.append("|   " * n + "|")
-    grid_lines.append("+---" * n + "+")
-
-    grid = "\n".join(grid_lines)
-
-    print(grid)
-
+    text = event.get("queryStringParameters", {}) .get("text", "Hello")
+    fig = Figlet(font="standard")
+    art = fig.renderText(text)
     return {
         "statusCode": 200,
-        "body": grid 
+        "body": art
     }
