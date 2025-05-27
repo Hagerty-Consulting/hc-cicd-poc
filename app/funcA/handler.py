7f4f7a883dee7c1050530a60703380a47f8e029f
Victor Pan <victor.pan@hagertyconsulting.com>
2025-05-27 10:30:56 -0400

    test

--- app/funcA/handler.py
+++ app/funcA/handler.py
@@ -1,10 +1,10 @@
 import requests
 
 def lambda_handler(event, context):
-    # fetch a random cat fact
-    resp = requests.get("https://catfact.ninja/fact")
+    
+    resp = requests.get("https://catfact.ninja/fact") 
     fact = resp.json().get("fact", "No fact found.")
-    print("Hello world!")
+
     return {
         "statusCode": 200,
         "body": fact
